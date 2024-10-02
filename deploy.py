9e730514f777121e408a7bc2d697896e8f0f4c94
Aditya Mohan <aditya530026@gmail.com>
2024-10-02 17:08:05 +0530

    init files

---
+++ deploy.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def deploy_honeypot():
+    print("Deploying SSH Honeypot using Docker Compose...")
+    subprocess.run(["docker-compose", "up", "--build", "-d"])
+
+if __name__ == "__main__":
+    deploy_honeypot()
