dbfec1a2e51be5145df841f40d1976876857b8f7
Aditya Mohan <aditya530026@gmail.com>
2024-10-02 17:17:29 +0530

    minor fix

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import subprocess
 
 def deploy_honeypot():
     print("Deploying SSH Honeypot using Docker Compose...")
-    subprocess.run(["docker-compose", "up", "--build", "-d"])
+    subprocess.run(["docker compose", "up", "--build", "-d"])
 
 if __name__ == "__main__":
     deploy_honeypot()
